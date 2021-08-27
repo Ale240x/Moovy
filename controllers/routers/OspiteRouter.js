4d27ed179d9fb21105c040d1a2ea77d885bf53e8
Merina00 <you@example.com>
2021-08-27 15:41:15 +0200

    commit on main

--- controllers/routers/OspiteRouter.js
+++ controllers/routers/OspiteRouter.js
@@ -16,12 +16,19 @@ var router = express.Router();
 
 router.get("", UtenteController.getSchermataIniziale);
 
-//Regione Registrazione Cliente
+//Regione Registrazione 
 router.get("/registrazione", UtenteController.getRegistrazioneCliente);
 router.post("/registrazione", UtenteController.postRegistrazioneCliente);
 
 //Regione Autenticazione
 router.get("/autenticazione", UtenteController.getAutenticazione);
 router.post("/autenticazione", UtenteController.postAutenticazione);
+
+//Regione recupera password? e controllo codice nuova pass
+// Da aggiungere AL CONTROLLER OSPITE/UTENTE NON SO!
+router.get("/recuperaPass", UtenteController.getRecuperaPass);
+router.post("/recuperaPass",UtenteController.postRecuperaPass);
+router.post("/recuperaPass/codice", UtenteController.postCodice);
+router.post("/recuperaPass/codice/nuovaPass", UtenteController.postNuovaPass);
 
 module.exports = router;
